b5918e4d2fc291815e116ebee0659696324cc258
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-10-31 18:00:26 +0100

    [debug] added temp debug log

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import router from "./router";
 
 const app = createApp(App);
 
+const auth0options = {
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  authorizationParams: {
+    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+  },
+}
+console.log("auth0options::", auth0options, "#")
 app
   .use(router)
   .use(
-    createAuth0({
-      domain: import.meta.env.VITE_AUTH0_DOMAIN,
-      clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-      authorizationParams: {
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
-      },
-    })
+    createAuth0(auth0options)
   )
   .mount("#root");
