ce6b61916c39e910a8bc57bbcedb2669aa05aac1
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-11-28 21:15:34 +0100

    [feat] change project presentation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,15 @@
   <PageLayout>
     <div class="white_background">
       <h1 class="white_background">SamGIS: GIS machine learning POC</h1>
-      <h4 class="white_background">In this project 
-        (inspired by <a class="white_background underlined" href="https://github.com/opengeos/segment-geospatial/">segment-geospatial</a>)
-        I perform inference on geospatial data by making requests to a serverless API based on a machine learning model based on
-        <a class="white_background underlined" href="https://github.com/vietanhdev/samexporter/">SAM Exporter</a>.
+      <h4 class="white_background">
+        <p>SamGIS is an attempt to perform instance segmentation on geospatial data even without the 
+        use of dedicated graphics cards.</p>
+        <p>The user interact on a <a class="white_background underlined" href="https://leafletjs.com">leaflet</a> webmap choosing areas to recognize and
+        a serverless python backend responds with a geojson containing one or more recognized polygons within the initial webmap.</p>
+        <p>The backend perform machine learning inference using a Segment Anything model using 
+        <a class="white_background underlined" href="https://onnxruntime.com">ONNXRuntime</a> as runtime.</p>
+        <p>Check about implementation details on my <a class="white_background underlined" href="https://trinca.tornidor.com/projects/samgis-sam-exporter-for-GIS">blog</a>.</p>
       </h4>
-      <h4 class="white_background"><a class="white_background underlined" href="https://trinca.tornidor.com/projects/samgis-sam-exporter-for-GIS">Here</a> my project presentation.</h4>
     </div>
   </PageLayout>
 </template>
