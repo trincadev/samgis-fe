ef33928afc70b2d197633a45866bed2192f3eb98
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-12-09 19:19:45 +0100

    [style] start using tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <PageLayout>
+
     <div class="white_background">
-      <h1 class="white_background">SamGIS: segment Anything applied to geodata</h1>
+
+      <h1 class="bg-green-400 text-red-800">
+        Hello world!
+      </h1>
+      <h1 class="bg-white text-black">SamGIS: segment Anything applied to geodata</h1>
       <h4 class="white_background">
-        <p>SamGIS is an attempt to perform 
-          <a class="white_background underlined" href="https://developer.ibm.com/learningpaths/get-started-artificial-intelligence/ai-basics/ai-beginners-guide">machine learning</a>
-          <a class="white_background underlined" href="https://en.wikipedia.org/wiki/Image_segmentation">instance segmentation</a> on 
-          <a class="white_background underlined" href="https://en.wikipedia.org/wiki/Geographic_data_and_information">geospatial data</a>
+        <p>SamGIS is an attempt to perform
+          <a class="white_background underline" href="https://developer.ibm.com/learningpaths/get-started-artificial-intelligence/ai-basics/ai-beginners-guide">machine learning</a>
+          <span> </span>
+          <a class="white_background underline" href="https://en.wikipedia.org/wiki/Image_segmentation">instance segmentation</a> on
+          <a class="white_background underline" href="https://en.wikipedia.org/wiki/Geographic_data_and_information">geospatial data</a>
           even without the use of dedicated graphics cards.</p>
-        <p>The user interact on a <a class="white_background underlined" href="https://leafletjs.com">leaflet</a> webmap choosing areas to recognize and then
-        a serverless python backend responds with a <a class="white_background underlined" href="https://it.wikipedia.org/wiki/GeoJSON">geojson</a>
+        <p>The user interact on a <a class="white_background underline" href="https://leafletjs.com">leaflet</a> webmap choosing areas to recognize and then
+        a serverless python backend responds with a <a class="white_background underline" href="https://it.wikipedia.org/wiki/GeoJSON">geojson</a>
         containing one or more recognized polygons within the initial webmap.</p>
-        <p>The backend perform machine learning inference using a <a class="white_background" href="https://segment-anything.com">Segment Anything</a> model using 
-        <a class="white_background underlined" href="https://onnxruntime.ai">ONNXRuntime</a> as runtime.</p>
-        <p>Check about implementation details on my <a class="white_background underlined" href="https://trinca.tornidor.com/projects/samgis-sam-exporter-for-GIS">blog</a>.</p>
+        <p>The backend perform machine learning inference using a <a class="white_background" href="https://segment-anything.com">Segment Anything</a> model using
+        <a class="white_background underline" href="https://onnxruntime.ai">ONNXRuntime</a> as runtime.</p>
+        <p>Check about implementation details on my <a class="white_background underline" href="https://trinca.tornidor.com/projects/samgis-sam-exporter-for-GIS">blog</a>.</p>
       </h4>
     </div>
   </PageLayout>
@@ -24,9 +30,6 @@ import PageLayout from "@/components/page-layout.vue";
 </script>
 
 <style>
-.underlined {
-  text-decoration: underline;
-}
 .white_background {
   background-color: white;
   color: black;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./dist/index.html",
+    "./src/**/*.{vue,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
