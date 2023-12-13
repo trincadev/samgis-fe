40eb0cf4dfe2647b9b5fce2e6698050d1b67b7a1
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-12-13 22:36:10 +0100

    [refactor] move the sidebar code within its component

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,29 +1,11 @@
 <template>
   <div class="">
     <!-- Sidebar -->
-    <aside class="fixed left-0 top-0 z-40 h-screen w-72 border-r pr-2" aria-label="Sidenav" id="drawer-navigation">
-      <SideBarBrand class="pl-4 pt-4"/>
-      <div class="h-full overflow-y-auto">
-        <ul class="mt-5 space-y-2 border-t border-gray-200 pt-4 pl-4">
-          <li><SideBarTab path="/" label="Home" /></li>
-          <li><SideBarTab path="/blog" label="My blog" /></li>
-          <li><SideBarTab path="/docs" label="SamGIS Documentation" /></li>
-          <template v-if="isAuthenticated">
-            <li><SideBarTab path="/prediction-map" label="Prediction Map" /></li>
-            <li><SideBarTab path="/profile" label="User Profile" /></li>
-          </template>
-        </ul>
-      </div>
-      <ul class="absolute bottom-4 z-20 pl-4 lg:flex">
-        <li>
-          <LoginButton v-if="!isAuthenticated" class="w-56" />
-          <LogoutButton v-else class="w-56" />
-        </li>
-      </ul>
-    </aside>
+    <SideBar />
 
     <main class="h-auto ml-72">
       <div class="mb-4 grid grid-cols-1 gap-4 py-16 pl-2">
+        <!-- style 'z-index: 1001' here is needed to avoid override from leafletjs css -->
         <h1 class="fixed top-0 h-16 w-full bg-gray-200 pl-2 pt-4" style="z-index: 1001">{{  props.pageTitle }}</h1>
         <div class="pt-4">
         <slot></slot>
@@ -45,6 +27,7 @@ import LogoutButton from '@/components/buttons/LogoutButton.vue'
 import LoginButton from '@/components/buttons/LoginButton.vue'
 import SideBarTab from '@/components/navigation/desktop/SideBarTab.vue'
 import SideBarBrand from '@/components/navigation/desktop/SideBarBrand.vue'
+import SideBar from '@/components/navigation/desktop/SideBar.vue'
 
 const { isAuthenticated } = useAuth0();
 
--- src/components/navigation/desktop/SideBar.vue
+++ src/components/navigation/desktop/SideBar.vue
@@ -1,28 +1,32 @@
 <template>
-  <!-- style 'z-index: 1001' here is needed to avoid override from leafletjs css -->
-  <ul class="bg-gray-200 fixed" style="z-index: 1001">
-    <li class="float-left"><NavBarBrand /></li>
-    <li class="float-left"><NavBarTab path="/" label="Home" /></li>
-    <li class="float-left"><NavBarTab path="/blog" label="My blog" /></li>
-    <li class="float-left"><NavBarTab path="/docs" label="SamGIS Documentation" /></li>
-    <template v-if="isAuthenticated">
-      <li class="float-left"><NavBarTab path="/prediction-map" label="Prediction Map" /></li>
-      <li class="float-left"><NavBarTab path="/profile" label="User Profile" /></li>
-    </template>
-
-    <li class="float-right">
-      <LoginButton v-if="!isAuthenticated" />
-      <LogoutButton v-else />
-    </li>
-  </ul>
+  <aside class="fixed left-0 top-0 z-40 h-screen w-72 border-r pr-2" aria-label="Sidenav" id="drawer-navigation">
+    <SideBarBrand class="pl-4 pt-4"/>
+    <div class="h-full overflow-y-auto">
+      <ul class="mt-5 space-y-2 border-t border-gray-200 pt-4 pl-4">
+        <li><SideBarTab path="/" label="Home" /></li>
+        <li><SideBarTab path="/blog" label="My blog" /></li>
+        <li><SideBarTab path="/docs" label="SamGIS Documentation" /></li>
+        <template v-if="isAuthenticated">
+          <li><SideBarTab path="/prediction-map" label="Prediction Map" /></li>
+          <li><SideBarTab path="/profile" label="User Profile" /></li>
+        </template>
+      </ul>
+    </div>
+    <ul class="absolute bottom-4 z-20 pl-4 lg:flex">
+      <li>
+        <LoginButton v-if="!isAuthenticated" class="w-56" />
+        <LogoutButton v-else class="w-56" />
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <script setup lang="ts">
-import NavBarBrand from "@/components/navigation/desktop/SideBarBrand.vue";
-import NavBarTab from '@/components/navigation/desktop/SideBarTab.vue'
 import { useAuth0 } from '@auth0/auth0-vue'
 import LogoutButton from '@/components/buttons/LogoutButton.vue'
 import LoginButton from '@/components/buttons/LoginButton.vue'
+import SideBarTab from '@/components/navigation/desktop/SideBarTab.vue'
+import SideBarBrand from '@/components/navigation/desktop/SideBarBrand.vue'
 
 const { isAuthenticated } = useAuth0();
 </script>
