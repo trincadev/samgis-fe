8210b03de7c0611eae5ebded31d62f5a6863ab03
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-12-22 16:50:27 +0100

    [style] fix h1 title in PageLayout on very small portrait viewports

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -4,7 +4,7 @@
     <SideBar />
 
     <main id="content" class="flex-1 z-1 pb-12 ml-[3.3rem] lg:ml-0 mr-4 overflow-y-auto lg:space-y-6 md:pl-1 lg:h-screen">
-      <header class="hidden md:flex md:mb-1 portrait:flex portrait:mb-1 items-center justify-between h-6 portrait:md:h-16 bg-gray-200 border-b sd:h-10">
+      <header class="hidden md:flex md:mb-1 portrait:flex portrait:mb-1 items-center justify-between h-10 portrait:md:h-16 bg-gray-200 border-b">
         <h1 class="hidden portrait:md:block">{{  props.pageTitle }}</h1>
         <h2 class="block text-sm font-bold portrait:sd:hidden">{{  props.pageTitle }}</h2>
       </header>
