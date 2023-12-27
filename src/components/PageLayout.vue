43127e1ec562abe89a52cbd05c9112798a8138bc
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2023-12-27 13:34:36 +0100

    [style] fix page layout title style

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -4,9 +4,8 @@
     <SideBar />
 
     <main id="content" class="flex-1 z-1 pb-12 ml-[3.3rem] lg:ml-0 mr-4 overflow-y-auto lg:space-y-6 md:pl-1 lg:h-screen">
-      <header class="hidden md:flex md:mb-1 portrait:flex portrait:mb-1 items-center justify-between h-10 portrait:md:h-16 bg-gray-200 border-b">
-        <h1 class="hidden portrait:md:block">{{  props.pageTitle }}</h1>
-        <h2 class="block text-sm font-bold portrait:sd:hidden">{{  props.pageTitle }}</h2>
+      <header class="hidden items-center justify-between h-10 landscape:md:flex portrait:sd:flex md:mb-1 portrait:mb-1 portrait:md:h-16 lg:h-16 bg-gray-200 border-b">
+        <h1 class="hidden sd:text-sm md:block md:text-xl lg:text-3xl">{{  props.pageTitle }}</h1>
       </header>
 
       <div class="">
